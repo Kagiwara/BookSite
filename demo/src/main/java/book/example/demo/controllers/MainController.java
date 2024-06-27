6ec1f993784b52fd79b2f56a1decfb2fa3164301
Dima <bordyur32@mail.ru>
2024-06-27 17:20:12 +0300

    запуск сервера и настройка страниц

---
+++ demo/src/main/java/book/example/demo/controllers/MainController.java
@@ -0,0 +1,22 @@
+package book.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/reviews")
+    public String reviews(Model model) {
+        return "reviews";
+    }
+
+}
